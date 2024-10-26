345696798edb90f364b51636677e59f059fb4cc4
Angel <alielali.py@gmail.com>
2024-10-26 18:52:34 -0400

    Add Lines

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from fpdf import FPDF
 import pandas as pd
 
-df = pd.read_csv('topics.csv')
 pdf = FPDF(orientation='P', unit='mm', format='A4')
+
 pdf.set_auto_page_break(auto=False, margin=0)
 
+df = pd.read_csv('topics.csv')
+
 for index, row in df.iterrows():
-    # Set up the header
     pdf.add_page()
+
+    # Set up the header
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=17, txt=row['Topic'], border=0, ln=1, align='L')
+
+    for i in range(23,285,10):
+        pdf.line(10,i, 200, i)
     pdf.line(10, 23, 200, 23)
 
     pdf.ln(260)
@@ -18,11 +24,14 @@ for index, row in df.iterrows():
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=10, txt=row['Topic'], border=0, ln=1, align='R')
 
+
     for i in range(row['Pages'] - 1):
         pdf.add_page()
         pdf.ln(277)
         pdf.set_font(family="Times", style="I", size=9)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row['Topic'], border=0, ln=1, align='R')
+        for i in range(10, 285, 10):
+            pdf.line(10, i, 200, i)
 
 pdf.output("output.pdf")
